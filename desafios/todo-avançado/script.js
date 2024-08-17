24b543792134cc8ae8eb28dd77188ab322f22379
João Emanuel <emanueljoaoss123@gmail.com>
2024-08-17 18:30:49 -0300

    calma

--- desafios/todo-avançado/script.js
+++ desafios/todo-avançado/script.js
@@ -8,12 +8,23 @@ const todofm = document.querySelector('input#itarefa')
 
 // FUNÇÕES
 
+function tarefa_adicionada(reload) {
+    reload.preventDefault()
+    let adicionar_tarefa_value = adicionar_tarefa.value
+
+    if (adicionar_tarefa_value){
+        salvar_tarefa(adicionar_tarefa_value)
+    }
+    console.log(salvar_tarefa())
+}
+
+function salvar_tarefa(){
+
+    
+}
+
 // EVENTOS 
 const evento_adicionar_tarefa = document.querySelector('#botao-adicionar-tarefa')
 evento_adicionar_tarefa.addEventListener('click', tarefa_adicionada)
 
-function tarefa_adicionada(reload) {
-    reload.preventDefault()
-    let adicionar_tarefa_value = adicionar_tarefa.value
-    console.log(adicionar_tarefa_value)
-}
+

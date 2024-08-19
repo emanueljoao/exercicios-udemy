705638cb529806873d90e7cea5832784024ce50d
João Emanuel <emanueljoaoss123@gmail.com>
2024-08-19 13:47:55 -0300

    começando o javascript

---
+++ desafios/todo-avançado/h.js
@@ -0,0 +1,11 @@
+function uma() {
+    console.log("megan fox prime");
+    sum(1, 2);
+}
+
+function sum(a, b) {
+    uma()
+    return a + b;
+}
+
+console.log(sum(1, 2))
--- desafios/todo-avançado/script.js
+++ desafios/todo-avançado/script.js
@@ -1,30 +1,85 @@
-// SELEÇÃO DE ELEMENTOS 
-const form_adiconar_tarefa = document.querySelector('.formulario-adicionar-tarefa')
-const adicionar_tarefa = document.querySelector('input#itarefa')
-const form_editar_tarefa = document.querySelector('.formulario-editar-tarefa')
-const editar_tarefa = document.querySelector('input#ieditar')
+//######################################################## SELEÇÃO DE ELEMENTOS #####################################################
+
+const formAddTask = document.querySelector('.formulario-adicionar-tarefa')
+const inputAddTask = document.querySelector('input#itarefa')
+const formEditTask = document.querySelector('.formulario-editar-tarefa')
+const editTask = document.querySelector('input#ieditar')
 const form_ = document.querySelector('.itarefa')
 const todofm = document.querySelector('input#itarefa')
 
-// FUNÇÕES
+//############################################################## FUNÇÕES ############################################################
 
-function tarefa_adicionada(reload) {
+
+//-------------------------------------------------- FUNÇÃO DE ADICIONAR A TAREFA ---------------------------------------------------
+function addTask(reload) {
     reload.preventDefault()
-    let adicionar_tarefa_value = adicionar_tarefa.value
+    const addTaskValue = inputAddTask.value
 
-    if (adicionar_tarefa_value){
-        salvar_tarefa(adicionar_tarefa_value)
+    if (addTaskValue){
+        saveTask(addTaskValue)
+        console.log(saveTask())
     }
-    console.log(salvar_tarefa())
 }
 
-function salvar_tarefa(){
-
-    
+function saveTask() {
+    const divTask = document.createElement('div')
+    divTask.classList('tarefa-pai')
+    addTaskChild(divTask)
 }
 
-// EVENTOS 
-const evento_adicionar_tarefa = document.querySelector('#botao-adicionar-tarefa')
-evento_adicionar_tarefa.addEventListener('click', tarefa_adicionada)
+function addTaskChild(divTask) {
+    const itemTask = document.createElement('div')
+    itemTask.classList('tarefa-filho')
+    divTask.appendChild(itemTask)
+    textTaskParam(itemTask)
+    containerButtons(itemTask)
+}
 
+function textTaskParam(itemTask) {
+    const textTask = document.createElement('p')
+    textTask.text = addTaskValue
+    itemTask.appendChild(textTask)
+}
+
+function containerButtons(itemTask) {
+    const divButtons = document.createElement('div')
+    divButtons.classList('buttons-edit-feito-clean')
+    itemTask.appendChild(divButtons)
+    buttons(divButtons)
+}
+
+function buttons(divButtons) {
+    const finishButton = document.createElement('button')
+    finishButton.classList('finish')
+    const editButton = document.createElement('button')
+    editButton.classList('editar')
+    const cleanButton = document.createElement('button')
+    cleanButton.classList('apagar')
+    divButtons.appendChild(finishButton, editButton, cleanButton)
+    icones()
+}
+
+function icones() {
+    const iconCheck = document.createElement('i')
+    iconCheck.classList('fa-solid', 'fa-check')
+    const iconEdit = document.createElement('i')
+    iconEdit.classList('fa-solid', 'fa-check')
+    const iconErased = document.createElement('i')
+    iconErased.classList('fa-solid', 'fa-xmark')
+}
+
+//---------------------------------------------- FECHAMENTO DA FUNÇÃO ADICIONAR TAREFA ----------------------------------------------
+
+
+
+//######################################################### EVENTOS ################################################################
+
+
+
+//--------------------------------------------------- EVENTO DE ADICIONAR TAREFA ----------------------------------------------------
+
+const eventAddTask = document.querySelector('#botao-adicionar-tarefa')
+eventAddTask.addEventListener('click', addTask)
+
+//----------------------------------------- FECHAMENTO DO EVENTO DE ADICIONAR TAREFA ------------------------------------------------
 
